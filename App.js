678f7d75aeb160547349538a44fe51c200673886
Max <maximumz160@gmail.com>
2021-06-14 11:10:19 +0700

    UI/UX

--- App.js
+++ App.js
@@ -1,21 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Home, OrderDelivery, Restaurant } from './screens';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import tabs from './navigation/tabs';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown:false}} initialRouteName="Home">
+        <Stack.Screen name="Home" component={tabs}/>
+        <Stack.Screen name="Restaurant" component={tabs}/>
+        <Stack.Screen name="OrderDelivery" component={tabs}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ constants/maps.js
@@ -0,0 +1,3 @@
+const GOOGLE_API_KEY = "AIzaSyCFG7BrzCoAqaEGQHVlVTRALQYehgsL294"
+
+export default GOOGLE_API_KEY
